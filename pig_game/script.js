e0d15324a417f79e7314cb4a07db49d6c7e358f5
whittlialleman <alle.alleman@gmail.com>
2023-05-31 13:59:09 -0600

    Finished video 82

--- pig_game/script.js
+++ pig_game/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//These both work the same, different ways to select elements by ID
+const score0El = document.querySelector('#score--0'); 
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+//Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
