6767b50cfaadea7af2d487933eeb748522bef9a4
whittlialleman <alle.alleman@gmail.com>
2023-05-31 11:11:16 -0600

    Finished video 73

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Correct Number!'
@@ -9,3 +9,13 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function() { //We don't call this function anywhere - the click event is what calls it
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(typeof guess);
+
+    if(!guess) {
+        document.querySelector('.message').textContent = 'No number!';
+    }
+});
