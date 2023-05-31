7484a8888725e303bb95f6d2bdf34f41d02a0a4d
whittlialleman <alle.alleman@gmail.com>
2023-05-31 11:51:42 -0600

    Finished video 76 - coding challenge 1

--- script.js
+++ script.js
@@ -11,9 +11,8 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1; //This cuts off the decimal points, sets the range between 1 and 20, and includes 20 in the range
+let secretNumber = Math.trunc(Math.random() * 20) + 1; //This cuts off the decimal points, sets the range between 1 and 20, and includes 20 in the range
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() { //We don't call this function anywhere - the click event is what calls it
     const guess = Number(document.querySelector('.guess').value);
@@ -26,6 +25,7 @@ document.querySelector('.check').addEventListener('click', function() { //We don
         //When player wins
     } else if(guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number!';
+        document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347'; //Changes to the DOM have to be in a string
         document.querySelector('.number').style.width = '30rem';
 
@@ -49,4 +49,18 @@ document.querySelector('.check').addEventListener('click', function() { //We don
         document.querySelector('.score').textContent = 0;
         }
     }
+});
+
+/////////////////////////////////
+//Coding Challenge 1
+
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
 });
