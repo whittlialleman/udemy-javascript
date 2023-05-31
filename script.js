2b3b49afe51c8f79c153943c131cb31193d89fd7
whittlialleman <alle.alleman@gmail.com>
2023-05-31 11:27:56 -0600

    Finished video 74

--- script.js
+++ script.js
@@ -11,11 +11,33 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1; //This cuts off the decimal points, sets the range between 1 and 20, and includes 20 in the range
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function() { //We don't call this function anywhere - the click event is what calls it
     const guess = Number(document.querySelector('.guess').value);
     console.log(typeof guess);
 
     if(!guess) {
         document.querySelector('.message').textContent = 'No number!';
+    } else if(guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number!';
+    } else if(guess > secretNumber) {
+        if(score > 1) {document.querySelector('.message').textContent = 'Too High!';
+        score--;
+        document.querySelector('.score').textContent = score;
+        } else {
+        document.querySelector('.message').textContent = 'You lost the game!';
+        document.querySelector('.score').textContent = 0;
+        }  
+    } else if(guess < secretNumber) {
+        if(score > 1) {document.querySelector('.message').textContent = 'Too Low!';
+        score--;
+        document.querySelector('.score').textContent = score;
+        } else {
+        document.querySelector('.message').textContent = 'You lost the game!';
+        document.querySelector('.score').textContent = 0;
+        }
     }
 });
