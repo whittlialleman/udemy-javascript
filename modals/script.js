8aea97c9dd0ce0588cdfa211caf71a6c3fc88708
whittlialleman <alle.alleman@gmail.com>
2023-05-31 13:45:33 -0600

    Finished video 81 - Finished project 2

--- modals/script.js
+++ modals/script.js
@@ -6,7 +6,6 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () { //Use these functions to help keep code dry - list the function later without the parentheses
-    console.log(('Button Clicked'));
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 };
@@ -22,3 +21,10 @@ for(let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function(e) {
+    console.log(e.key);
+
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
+            closeModal();
+    }
+});
