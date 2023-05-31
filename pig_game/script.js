58f582e0201c60247ae5419058b6231b68c96a5f
whittlialleman <alle.alleman@gmail.com>
2023-05-31 14:41:22 -0600

    Finished video 85

--- pig_game/script.js
+++ pig_game/script.js
@@ -21,12 +21,21 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () { //Because nothing changes in the two places this will be called, no parameter is needed
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
+}
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function() {
+    if(playing) {
     //1. Generate a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice);
 
     //2. Display dice
     diceEl.classList.remove('hidden');
@@ -39,10 +48,27 @@ btnRoll.addEventListener('click', function() {
         document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
         //Switch to next player
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
+        switchPlayer();
+    }
+    }
+});
+
+btnHold.addEventListener('click', function () {
+    if(playing) {
+    //1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+    //2. Check if players score is greater than or equal to 100
+    if(scores[activePlayer] >= 20) {
+        //Finish game
+        playing = false;
+        diceEl.classList.add('hidden');
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    } else {
+        //Switch to the next player
+        switchPlayer();
+    }
     }
 });
