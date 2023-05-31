19a28b4e6e4c6850233d470d2e4521240772a5f2
whittlialleman <alle.alleman@gmail.com>
2023-05-31 11:57:16 -0600

    Finished video 77

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1; //This cuts off the decimal points, sets the range between 1 and 20, and includes 20 in the range
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function() { //We don't call this function anywhere - the click event is what calls it
     const guess = Number(document.querySelector('.guess').value);
@@ -29,6 +30,10 @@ document.querySelector('.check').addEventListener('click', function() { //We don
         document.querySelector('body').style.backgroundColor = '#60b347'; //Changes to the DOM have to be in a string
         document.querySelector('.number').style.width = '30rem';
 
+        if(score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
         //When guess is too high
     } else if(guess > secretNumber) {
         if(score > 1) {document.querySelector('.message').textContent = 'Too High!';
