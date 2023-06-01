f1a5e7726da7d8f71818e2e8355f8f861f3c6b79
whittlialleman <alle.alleman@gmail.com>
2023-06-01 12:33:17 -0600

    Finished video 104

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+/////////////////////////////////////////////////
+//Destructuring Arrays
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -11,11 +13,6 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  order: function(starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-
   openingHours: {
     thu: {
       open: 12,
@@ -30,35 +27,79 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`);
+  },
 };
 
-const arr = [2,3,4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
 
-const [x,y,z] = arr; //This is called destructuring - the const looks like an array but it's not
-console.log(x,y,z);
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+  starterIndex: 1,
+})
 
-let [main, , secondary] = restaurant.categories;
-console.log(main, secondary);
+// const arr = [2,3,4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
 
-//Switching variables
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
+// const [x,y,z] = arr; //This is called destructuring - the const looks like an array but it's not
+// console.log(x,y,z);
 
-//Receive two return values from a function
-//console.log(restaurant.order(2,0)); //log shows Garlic Bread and Pizza
-const [starter, mainCourse] = restaurant.order(2,0);
-console.log(starter, mainCourse);
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
 
-//Nested values
-const nested = [2, 4, [5, 6]];
-// const [i, , j] = nested;
-// console.log(i, j);
-const [i, , [j, k]] = nested;
-console.log(i, j, k);
+// //Switching variables
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
 
-//Default Values
-const [p=1, q=1, r=1] = [8, 9];
-console.log(p,q,r);
+// //Receive two return values from a function
+// //console.log(restaurant.order(2,0)); //log shows Garlic Bread and Pizza
+// const [starter, mainCourse] = restaurant.order(2,0);
+// console.log(starter, mainCourse);
+
+// //Nested values
+// const nested = [2, 4, [5, 6]];
+// // const [i, , j] = nested;
+// // console.log(i, j);
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// //Default Values
+// const [p=1, q=1, r=1] = [8, 9];
+// console.log(p,q,r);
+
+///////////////////////////////////////////
+//Destructuring Objects
+
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//Default values
+const {menu = [], starterMenu: starters = []} = restaurant;
+console.log(menu, starters);
+
+//Mutating variables
+let a = 111;
+let b = 999;
+const obj = {a: 23, b: 7, c: 14};
+
+({a, b} = obj); //Have to put this in parentheses or else JS won't let it work because it thinks the curly braces are a code block and you can't set a code block equal to a variable
+console.log(a,b);
+
+//Nested objects
+const {fri: {open, close}} = openingHours;
+console.log(open, close);
