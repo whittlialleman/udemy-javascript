080ec7b453c4f76e4ce4552ac6d2cee51105f983
whittlialleman <alle.alleman@gmail.com>
2023-06-05 14:29:27 -0600

    Finished video 110 - coding challenge 1

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ const restaurant = {
   },
 };
 
+/*
 ////////////////////////////////////
 //Logical Assignment Operators
 
@@ -80,7 +81,6 @@ rest2.owner &&= '<ANONYMOUS>';
 console.log(rest1);
 console.log(rest2);
 
-/*
 ////////////////////////////////////
 //Nullish Coalescing Operator
 
@@ -274,3 +274,72 @@ console.log(restaurant.name);
 // const {fri: {open, close}} = openingHours;
 // console.log(open, close);
 */
+
+////////////////////////////////////////
+//Coding Challenge 1
+
+const game = {
+  team1: 'Brayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+  [
+    'Burki',
+    'Schulz',
+    'Hummels',
+    'Akanji',
+    'Hakimi',
+    'Weigl',
+    'Witsel',
+    'Hazard',
+    'Brandt',
+    'Sancho',
+    'Gotze',
+  ],
+],
+score: '4:0',
+scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+date: 'Nov 9th, 2037',
+odds: {
+  team1: 1.33,
+  x: 3.25,
+  team2: 6.5,
+},
+};
+
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+const {odds: {team1, x: draw, team2}} = game;
+console.log(team1, draw, team2);
+
+const printGoals = function(...players) {
+  console.log(`${players.length} goals were scored`);
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals('Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+team1 < team2 && console.log('Team 1 is more likely to win');
