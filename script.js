6f4d0dbd07447b5efab6c00825b05ae82e0cd56d
whittlialleman <alle.alleman@gmail.com>
2023-06-05 13:23:37 -0600

    Finished video 106

--- script.js
+++ script.js
@@ -37,9 +37,53 @@ const restaurant = {
 
   orderPasta: function (ing1, ing2, ing3) {
     console.log(`Here is your delivious pasta with ${ing1}, ${ing2}, and ${ing3}.`);
-  }
+  },
+
+  orderPizza: function(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
+//////////////////////////////////////
+//Rest Pattern and Parameters
+
+//1) Destructuring
+
+//SPREAD, because the dots are on the right side of the = sign
+const arr = [1, 2, ...[3, 4]];
+
+//REST, because the dogs are on the left side of the = sign
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(pizza, risotto, otherFood);
+
+//Objects
+const {sat, ...weekdays} = restaurant.openingHours;
+console.log(weekdays);
+
+//2) Functions
+const add = function(...numbers) {
+  let sum = 0;
+  for (let i=0; i<numbers.length; i++) {
+    sum += numbers[i];
+  };
+  console.log(sum);
+};
+
+add(2,3);
+add(5,3,7,2);
+add(8,2,5,3,2,1,4);
+
+const x = [23,5,7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
+
+/*
 ////////////////////////////////////////
 //The Spread Operator
 
@@ -85,7 +129,6 @@ restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
 
-/*
 // restaurant.orderDelivery({
 //   time: '22:30',
 //   address: 'Via del Sole, 21',
