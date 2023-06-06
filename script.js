1fad5de4ba038ae3194b8755465a35b948f792b3
whittlialleman <alle.alleman@gmail.com>
2023-06-06 15:07:31 -0600

    Finished video 118

--- script.js
+++ script.js
@@ -52,6 +52,46 @@ const restaurant = {
   },
 };
 
+////////////////////////////////////////
+//Maps: Iteration
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Way to go!'],
+  [false, 'Back to the books'],
+]);
+console.log(question);
+
+//Convert object to map
+console.log(Object.entries(openingHours));
+
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if(typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  };
+};
+
+const answer = 3 //Number(prompt('Your answer'));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+//Convert map to array
+console.log([...question]);
+//console.log(question.entries()); //This gives the same info as the line above
+console.log(question.keys());
+console.log(question.values());
+
+/*
 /////////////////////////////////////////
 //Maps: Fundamentals
 
@@ -86,7 +126,6 @@ console.log(rest.size);
 
 console.log(rest.get(arr));
 
-/*
 ////////////////////////////////////////
 //Sets
 
