5a914d7994736c433ea6baf5160815b1377c1a32
whittlialleman <alle.alleman@gmail.com>
2023-06-07 15:39:11 -0600

    Finished video 128

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function(flightNum, numPassengers = 1, price = 199 * numPassengers) {
+    
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    }
+    console.log(booking);
+    bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 800); //This sets it back to the default value
