f90b089b9c4d332d4fc64cf196380f131377da7a
whittlialleman <alle.alleman@gmail.com>
2023-06-07 15:50:20 -0600

    Finished video 129

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
-
+/*
+//////////////////////////////////////////
+//Default parameters
 const bookings = [];
 
 const createBooking = function(flightNum, numPassengers = 1, price = 199 * numPassengers) {
@@ -18,3 +20,34 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 800); //This sets it back to the default value
+*/
+
+/////////////////////////////////////////
+//How passing arguments works: Value vs. Reference
+const flight = 'LH234';
+const whip = {
+    name: 'Whittli Alleman',
+    passport: 2048573894,
+}
+
+const checkIn = function(flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mrs. ' + passenger.name;
+
+    if (passenger.passport === 2048573894) {
+        alert('Checked in');
+    } else {
+        alert('Wrong passport!');
+    };
+};
+
+// checkIn(flight, whip);
+// console.log(flight);
+// console.log(whip);
+
+const newPassport = function(person) {
+    person.passport = Math.trunc(Math.random() * 10000000000000)
+};
+
+newPassport(whip);
+checkIn(flight, whip);
