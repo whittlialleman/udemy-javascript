421484bd551aba2f70e317874697f3c26db81ea6
whittlialleman <alle.alleman@gmail.com>
2023-06-12 14:05:53 -0600

    Finished video 154 - coding challenge 2

--- script.js
+++ script.js
@@ -272,7 +272,6 @@ const withdrawals = movements.filter(function(mov) {
   return mov < 0;
 });
 console.log(withdrawals);
-*/
 
 ///////////////////////////////////
 //Reduce Method
@@ -301,3 +300,19 @@ const max = movements.reduce((acc, mov) => {
   }
 }, movements[0]);
 console.log(max);
+*/
+ //////////////////////////////////////////////////////
+ //Coding CHallenge 2
+
+ const calcAverageHumanAge = function(ages) {
+  const humanAges = ages.map(age => age <= 2 ? (2 * age) : (16 + age *4));
+  const adults = humanAges.filter(age => age >= 18);
+  console.log(humanAges);
+  console.log(adults);
+
+  const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+  return average;
+ };
+const test1 = calcAverageHumanAge([5,2,4,1,15,8,3]);
+const test2 = calcAverageHumanAge([16,6,10,5,6,1,4]);
+console.log(test1, test2);
