1f0d29ecc9e6db76236158d50436d52029317ce1
whittlialleman <alle.alleman@gmail.com>
2023-06-12 14:40:35 -0600

    Finished video 157

--- script.js
+++ script.js
@@ -344,7 +344,6 @@ const eurToUSD = 1.1;
 //Pipeline
 const totalDepositsUSD = movements.filter(mov => mov > 0).map(mov => mov * eurToUSD).reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
-*/
 
 ////////////////////////////////////////
 //Coding Challenge 3
@@ -353,3 +352,17 @@ const calcAverageHumanAge = ages => ages.map(age => age <= 2 ? (2 * age) : (16 +
 const test1 = calcAverageHumanAge([5,2,4,1,15,8,3]);
 const test2 = calcAverageHumanAge([16,6,10,5,6,1,4]);
 console.log(test1, test2);
+*/
+
+/////////////////////////////////////////
+//Find Method
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === "Jessica Davis");
+console.log(account);
