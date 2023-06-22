0beed0627794c0b7915c4a3b678b9bd6bf2efaf3
whittlialleman <alle.alleman@gmail.com>
2023-06-22 08:49:15 -0600

    Finished video 186

--- script.js
+++ script.js
@@ -29,3 +29,37 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/////////////////////////////////////////////
+//Selecting, Creating, and Deleting Elements
+
+//Selecting Elements
+console.log(document.documentElement);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1'); 
+const allButtons = document.getElementsByTagName('button'); //This shows an HTML collection
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+//Creating and inserting elements
+
+const message = document.createElement('div'); //This creates it but it isn't on the page anywhere because we haven't inserted it
+message.classList.add('cookie-message');
+//message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// header.prepend(message); //Prepending adds it as the first child element, append adds it as the last child
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+//Delete Elements
+document.querySelector('.btn--close-cookie').addEventListener('click', function() {
+  message.remove();
+});
