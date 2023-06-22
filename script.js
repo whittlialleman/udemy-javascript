411ba3587206e1195464f42fef2a27abb171e58a
whittlialleman <alle.alleman@gmail.com>
2023-06-22 11:34:57 -0600

    Finished video 198

--- script.js
+++ script.js
@@ -179,6 +179,30 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 headerObserver.observe(header);
 
 //////////////////////////////////////////////
+//Reveal Sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function(entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if(!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target); //Keeps the observer entries from being logged in the console
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function(section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+//////////////////////////////////////////////
 //////////////////////////////////////////////
 //////////////////////////////////////////////
 //Lecture code-alongs
