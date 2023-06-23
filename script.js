1f492ba92a8b7b814ce11ef8b21e24526ddf3e10
whittlialleman <alle.alleman@gmail.com>
2023-06-23 09:34:10 -0600

    Finished video 214

--- script.js
+++ script.js
@@ -99,7 +99,6 @@ bmw.brake();
 bmw.accelerate();
 mercedes.brake();
 mercedes.accelerate();
-*/
 
 ///////////////////////////////////////////
 //ES6 Classes
@@ -137,3 +136,59 @@ jessica.greet();
 //1. Classes are not hoisted
 //2. Classes are first-class citizens
 //3. Classes are executed in strict mode (even if it's not declared at the top of the file)
+*/
+
+////////////////////////////////////
+//Getters and Setters
+
+const account = {
+    owner: 'Whip',
+    movements: [200, 530, 120, 300],
+
+    get latest() {
+        return this.movements.slice(-1).pop();
+    },
+
+    set latest(mov) {
+        this.movements.push(mov);
+    },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
+
+class PersonCl {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    };
+
+    //methods will be added to .prototype property
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    };
+
+    greet() {
+        console.log(`Hey ${this.firstName}`);
+    };
+
+    get age() {
+        return 2037 - this.birthYear;
+    };
+
+    set fullName(name) {
+        if(name.includes(' ')) this._fullName = name;
+        else alert(`${name} is not a full name!`)
+    };
+
+    get fullName() {
+        return this._fullName;
+    };
+};
+
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica.age);
+
+const walter = new PersonCl('Walter White', 1965);
