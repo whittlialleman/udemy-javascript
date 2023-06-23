38800e02520a3ee87be1ff2c7778cf5dc917befc
whittlialleman <alle.alleman@gmail.com>
2023-06-23 11:08:11 -0600

    Finished video 221

--- script.js
+++ script.js
@@ -373,7 +373,6 @@ console.log(elecCar);
 
 elecCar.accelerate();
 elecCar.chargeBattery(50);
-*/
 
 //////////////////////////////////////
 //Inheritance between classes: ES6 Classes
@@ -432,3 +431,36 @@ class StudentCl extends PersonCl {
 const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 martha.introduce();
 martha.calcAge();
+*/
+
+//////////////////////////////////////
+//Inheritance between classes: ES6 Classes
+
+const PersonProto = {
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    },
+
+    init(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function(firstName, birthYear, course) {
+    PersonProto.init.call(this, firstName, birthYear);
+    this.course = course;
+}
+
+StudentProto.introduce = function() {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);
+}
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
