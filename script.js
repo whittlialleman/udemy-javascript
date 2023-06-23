989bbd96ad06638f28f4c3d1765adfa3a509513e
whittlialleman <alle.alleman@gmail.com>
2023-06-23 08:44:17 -0600

    Finished video 209

--- script.js
+++ script.js
@@ -27,3 +27,28 @@ const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
 
 console.log(whip instanceof Person);
+
+/////////////////////////////////////
+//Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function() {
+    console.log(2037 - this.birthYear);
+};
+
+whip.calcAge();
+matilda.calcAge();
+jack.calcAge();
+
+console.log(whip.__proto__);
+console.log(whip.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(whip)); //true
+console.log(Person.prototype.isPrototypeOf(matilda)); //true
+console.log(Person.prototype.isPrototypeOf(Person)); //false - it is the prototype of the linked objects, not of itself
+
+Person.prototype.species = "Homo Sapiens";
+console.log(whip, matilda);
+
+console.log(whip.hasOwnProperty('firstName')); //true
+console.log(whip.hasOwnProperty('species')); //false
