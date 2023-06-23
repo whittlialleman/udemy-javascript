bc7e45c6d310c7b73b0c5b68184c8d0de595f51f
whittlialleman <alle.alleman@gmail.com>
2023-06-23 11:01:04 -0600

    Finished video 220

--- script.js
+++ script.js
@@ -341,7 +341,6 @@ console.log(mike instanceof Object);
 
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
-*/
 
 //////////////////////////////////////
 //Coding challenge 3
@@ -374,3 +373,62 @@ console.log(elecCar);
 
 elecCar.accelerate();
 elecCar.chargeBattery(50);
+*/
+
+//////////////////////////////////////
+//Inheritance between classes: ES6 Classes
+
+class PersonCl {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    };
+
+    //methods will be added to .prototype property - instance methods
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    };
+
+    greet() {
+        console.log(`Hey ${this.firstName}`);
+    };
+
+    get age() {
+        return 2037 - this.birthYear;
+    };
+
+    set fullName(name) {
+        if(name.includes(' ')) this._fullName = name;
+        else alert(`${name} is not a full name!`)
+    };
+
+    get fullName() {
+        return this._fullName;
+    };
+
+    //Static method
+    static hey() {
+        console.log('Hey there!');
+        console.log(this);
+    }
+};
+
+class StudentCl extends PersonCl {
+    constructor(fullName, birthYear, course) {
+        //Always needs to happen first as this creates the this keyword
+        super(fullName, birthYear);
+        this.course = course;
+    }
+
+    introduce() {
+        console.log(`My name is ${this.fullName} and I study ${this.course}`);
+    }
+
+    calcAge() {
+        console.log(`I am ${2037 - this.birthYear} years old, but as a student, I feel more like ${2037 - this.birthYear + 30}`)
+    }
+};
+
+const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
+martha.introduce();
+martha.calcAge();
