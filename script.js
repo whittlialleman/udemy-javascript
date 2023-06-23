ea4874131c25beb2b6b65f093b8428ab4b8da29e
whittlialleman <alle.alleman@gmail.com>
2023-06-23 10:32:04 -0600

    Finished video 219 - coding challenge 3

--- script.js
+++ script.js
@@ -303,7 +303,6 @@ console.log(ford.speedUS);
 ford.accelerate();
 ford.speedUS = 50;
 console.log(ford);
-*/
 
 ///////////////////////////////////////
 //Inheritance between classes: constructor functions
@@ -342,3 +341,36 @@ console.log(mike instanceof Object);
 
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
+*/
+
+//////////////////////////////////////
+//Coding challenge 3
+
+const Car = function(make, speed) {
+    this.make = make;
+    this.speed = speed;
+};
+
+const EV = function(make, speed, charge) {
+    Car.call(this, make, speed);
+    this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function(chargeTo) {
+    this.charge = chargeTo;
+    console.log(`${this.make} is charged to ${this.charge}`);
+};
+
+EV.prototype.accelerate = function() {
+    this.speed += 20;
+    this.charge--;
+    console.log(`${this.make} is going ${this.speed} km/h, with a charge of ${this.charge}.`);
+}
+
+const elecCar = new EV('Tesla', 120, 23);
+console.log(elecCar);
+
+elecCar.accelerate();
+elecCar.chargeBattery(50);
