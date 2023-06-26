843807cecce5f3653d3adb1009672a0c96440265
whittlialleman <alle.alleman@gmail.com>
2023-06-26 14:00:10 -0600

    Finished video 238

--- script.js
+++ script.js
@@ -11,6 +11,51 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+    date = new Date();
+    id = (Date.now + '').slice(-10);
+
+    constructor(coords, distance, duration) {
+        this.coords = coords; //lat, lng
+        this.distance = distance; //miles
+        this.duration = duration; //minutes
+    }
+}
+
+class Running extends Workout {
+    constructor(coords, distance, duration, cadence) {
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+
+    calcPace() {
+        //min/mi
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+}
+class Cycling extends Workout {
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+    }
+
+    calcSpeed() {
+        //mi/h
+        this.speed = this.distance / (this.duration / 60);
+        return this.speed;
+    }
+}
+
+//Testing
+// const run1 = new Running([39, -12], 5.2, 24, 178)
+// const cycle1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cycle1);
+
+/////////////////////////////////
+//Application Architecture
 class App {
     #map;
     #mapEvent;
@@ -66,7 +111,7 @@ class App {
         const {lat, lng} = this.#mapEvent.latlng;
         L.marker([lat, lng]).addTo(this.#map).bindPopup(L.popup({maxWidth: 250, minWidth: 100, autoClose: false, closeOnClick: false, className: 'running-popup'})).setPopupContent('Workout').openPopup();
     }
-}
+};
 
 const app = new App();
 
