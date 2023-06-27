9c3dee8fb5f0f24d5538d33c2ef4777812b984e4
whittlialleman <alle.alleman@gmail.com>
2023-06-27 11:32:48 -0600

    Finished video 255

--- script.js
+++ script.js
@@ -94,22 +94,63 @@ setTimeout(() => {
 
 // getCountryData('usa');
 
+const getJSON = function(url, errorMsg = 'Something went wrong') {
+    return fetch(url).then(response => {if(!response.ok) {
+        throw new Error(`${errorMsg} (${response.status})`)
+    }
+    return response.json();
+    });
+};
 
+// const getCountryData = function(country) {
+//     //country 1
+//     fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(response => {
+//         console.log(response);
+
+//         if(!response.ok) {
+//             throw new Error(`Country not found (${response.status})`)
+//         }
+//         return response.json()
+//     })
+//     .then(data => {
+//         renderCountry(data[0]);
+//         //const neighbour = data[0].borders[0];
+//         const neighbour = 'asdfads';
+
+//         if(!neighbour) return;
+        
+//         //country 2
+//         return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     })
+//     .then(response => {
+//         if(!response.ok) {
+//             throw new Error(`Country not found (${response.status})`)
+//         }
+//         return response.json();
+//     })
+//     .then(data => renderCountry(data[0], 'neighbour'))
+//     .catch(err => {
+//         console.error(`${err}`);
+//         renderError(`Something went wrong. ${err.message}. Try again!`);
+//     })
+//     .finally(() => {
+//         countriesContainer.style.opacity = 1;
+//     });
+// };
 
 const getCountryData = function(country) {
     //country 1
-    fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(response => response.json())
+    getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
     .then(data => {
         renderCountry(data[0]);
         const neighbour = data[0].borders[0];
 
-        if(!neighbour) return;
+        if(!neighbour) throw new Error('No neighbor found!');
         
         //country 2
-        return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+        return getJSON(`https://restcountries.com/v3.1/alpha/${neighbour}`, 'Country not found');
     })
-    .then(response => response.json())
     .then(data => renderCountry(data[0], 'neighbour'))
     .catch(err => {
         console.error(`${err}`);
@@ -124,4 +165,4 @@ btn.addEventListener('click', function() {
     getCountryData('usa');
 });
 
-getCountryData('aksjdfsk'); //log shows cannot read property of undefined because the object doesn't exist so it can't pull data for it
+getCountryData('australia');
