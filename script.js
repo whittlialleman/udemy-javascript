a817620e355c2dddf4cfa1447d1c9bc786ff9215
whittlialleman <alle.alleman@gmail.com>
2023-06-27 11:52:41 -0600

    Finished video 256

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
@@ -24,7 +23,7 @@ const renderCountry = function(data, className) {
             </div>
         </article>`;
         countriesContainer.insertAdjacentHTML('beforeend', html);
-        // countriesContainer.style.opacity = 1;
+        countriesContainer.style.opacity = 1;
 }
 /*
 const getCountryAndNeighbor = function(country) {
@@ -74,7 +73,6 @@ setTimeout(() => {
         }, 1000);
     }, 1000);
 }, 1000);
-*/
 
 // const getCountryData = function(country) {
 //     fetch(`https://restcountries.com/v3.1/name/${country}`).then(function(response) {
@@ -166,3 +164,32 @@ btn.addEventListener('click', function() {
 });
 
 getCountryData('australia');
+*/
+
+////////////////////////////////////////////////////////////
+//Coding challenge 3
+const whereAmI = function(lat, lng) {
+    fetch(`https://geocode.xyz/${lat},&{lng?geoit=json`)
+    .then(res => {
+        if(!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+        return res.json();
+    })
+    .then(data => {
+        console.log(data);
+        console.log(`You are in ${data.city}, ${data.country}`);
+
+        return fetch(`https://restcountries.com/v3.1/name/${data.country}`)
+    })
+    .then(res => {
+        if(!res.ok) {
+            throw new Error(`Country not found (${res.status})`)
+        }
+        return res.json();
+        })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.error(`${err.message} emoji`));
+};
+
+whereAmI(52.508, 13.381);
+//whereAmI(19.307, 72.873);
+//whereAmI(-33.933, 18.474);
