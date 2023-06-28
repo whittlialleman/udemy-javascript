b8ee706bd79ad1f9cdea279b23c6cbcdab094eb0
whittlialleman <alle.alleman@gmail.com>
2023-06-28 09:48:09 -0600

    Finished video 265

--- script.js
+++ script.js
@@ -343,7 +343,6 @@ createImage('img/img-1.jpg').then(img => {
     return createImage('img/img-3.jpg');
 })
 .catch(err => console.error(err));
-*/
 
 ///////////////////////////////////////
 //Consuming promises with Async/Await
@@ -402,3 +401,31 @@ console.log('1: Will get location');
     }
     console.log('3: Finished getting location');
 })();
+*/
+
+/////////////////////////////////////////////
+//Running promises in parallel
+
+const getJSON = function(url, errorMsg = 'Something went wrong') {
+    return fetch(url).then(response => {if(!response.ok) {
+        throw new Error(`${errorMsg} (${response.status})`)
+    }
+    return response.json();
+    });
+};
+
+const get3Countries = async function(c1, c2, c3) {
+    try {
+        // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+        // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+        // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+        const data = await Promise.all([getJSON(`https://restcountries.com/v3.1/name/${c1}`), getJSON(`https://restcountries.com/v3.1/name/${c2}`), getJSON(`https://restcountries.com/v3.1/name/${c3}`)]);
+
+        console.log(data.map(d => d[0].capital[0]));
+    } catch(err) {
+        console.error(err);
+    }
+};
+
+get3Countries('portugal', 'usa', 'canada');
