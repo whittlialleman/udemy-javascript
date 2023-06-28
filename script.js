66f7a901258209cb98c4dd76d91b909c7171c096
whittlialleman <alle.alleman@gmail.com>
2023-06-28 11:39:47 -0600

    Finished video 274

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 //Importing module
 // import {addToCart, totalPrice as price, tq} from './shoppingCart.js';
 // console.log('Importing module');
@@ -39,3 +40,36 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+
+////////////////////////////////////////////////////
+//Module pattern
+
+const ShoppingCart2 = (function() {
+    const cart = [];
+    const shippingCost = 10;
+    const totalPrice = 237;
+    const totalQuantity = 23;
+
+    const addToCart = function(product, quantity) {
+        cart.push({product, quantity});
+        console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
+    };
+
+    const orderStock = function(product, quantity) {
+        cart.push({product, quantity});
+        console.log(`${quantity} ${product} ordered from supplier`)
+    };
+
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity,
+    };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost); //This does not work, log shows undefined
