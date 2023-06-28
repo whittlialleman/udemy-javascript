9d96de7e2b1faa61c1e789623ee5fb224c2ed1d7
whittlialleman <alle.alleman@gmail.com>
2023-06-28 11:24:25 -0600

    Finished video 273

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ add('breads', 5);
 add('apples', 4);
 
 console.log(cart);
+
+///////////////////////////////////////////////////
+//Top level await as of 2022 - no longer have to use await inside an async function
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
+
+    return {title: data.at(-1).title, text: data.at(-1).body};
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+//Not very clean
+//lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
